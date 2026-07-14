0192620366feabf98d93dcab32308f87e11565b3
shaurya aswal <shaurya.aswal12@gmail.com>
2026-07-14 13:01:00 +0530

    changed variable name

--- week1/project/resume.py
+++ week1/project/resume.py
@@ -22,55 +22,51 @@ model  = "llama-3.3-70b-versatile"
 
 # opening the data of the resume from the pdf file
 pdf_path = "/Users/apple/agentic ai/week1/project/utils/resume_.pdf"
-raw_data = PdfReader(pdf_path)
-input_prompt = raw_data.pages[0].extract_text()
+pdf_reader = PdfReader(pdf_path)
+resume_text = pdf_reader.pages[0].extract_text()
 
 # defining the schema for the resume using pydantic
 class Resume(BaseModel):
     Experience : int
     Skills : str
 
-schema = Resume.model_json_schema()
+resume_schema = Resume.model_json_schema()
 
-response_format = {
+json_response_format = {
     "type" : "json_object"
 }
 
 # defining the system prompt for the model
 
-system_prompt = f""" this is the resume of a person ,get the information from  resume and return it in json format  and the schema should be {schema} and values in json format"""
+resume_parser_system_prompt = f""" this is the resume of a person ,get the information from  resume and return it in json format  and the schema should be {resume_schema} and values in json format"""
 
 
 role = "user"
-prompt = f""" this is the resume of the person {input_prompt}"""
-user = {"role": role,"content": prompt}
+resume_parser_prompt = f""" this is the resume of the person {resume_text}"""
+user_message = {"role": role,"content": resume_parser_prompt}
 
-system = {"role": "system","content": system_prompt}
-messages = [system, user]
+system_message = {"role": "system","content": resume_parser_system_prompt}
+resume_parser_messages = [system_message , user_message]
  
 # info from the resume
-response = client.chat.completions.create(model = model,messages = messages,response_format=response_format)
-info = response.choices[0].message.content
+resume_response = client.chat.completions.create(model = model,messages = resume_parser_messages,response_format=json_response_format)
+resume_json = resume_response.choices[0].message.content
 
 ## matching the requirements with the resume
 class Match(BaseModel):
     eligible : str
     percentage : int
 
-match_schema = Match.model_json_schema()
+job_match_schema = Match.model_json_schema()
 
-resp_format = {
-    "type" : "json_object"
-}
 
-system_prompt_match = f"""the schema for the match is {match_schema} and values in json format ,check if the resume the requirements or not and return yes or no in json format with key eligible and value yes or no and also return the percentage out of 100 of match in json format with key percentage"""
-required = {"Experience": 1, "Skills": "python, machine learning, deep learning , genai"}
+job_match_system_prompt= f"""the schema for the match is {job_match_schema} and values in json format ,check if the resume the requirements or not and return yes or no in json format with key eligible and value yes or no and also return the percentage out of 100 of match in json format with key percentage"""
+job_requirements = {"Experience": 1, "Skills": "python, machine learning, deep learning , genai"}
+job_match_prompt = f""" this is the personal information of the person {resume_json} and these are the requirements for the job {job_requirements} """
 
-match_prompt = f""" this is the personal information of the person {info} and these are the requirements for the job {required} """
+match_user_message = {"role": role,"content": job_match_prompt}
+match_system_message= {"role": "system","content": job_match_system_prompt}
+job_match_messages = [match_system_message ,match_user_message]
+job_match_response = client.chat.completions.create(model = model,messages = job_match_messages,response_format=json_response_format )
 
-match_user = {"role": role,"content": match_prompt}
-match_system = {"role": "system","content": system_prompt_match}
-match_messages = [match_system, match_user]
-match_response = client.chat.completions.create(model = model,messages = match_messages,response_format=resp_format)
-
-print(match_response.choices[0].message.content)
+print(job_match_response.choices[0].message.content)
